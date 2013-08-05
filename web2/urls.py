23f49d3c22c64e73f76733938c4010cfa5576fe1
Nicolas Malbran <nmalbran@gmail.com>
2013-08-05 01:31:30 -0400

    Agregado historia.

--- web2/urls.py
+++ web2/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^/?$', TemplateView.as_view(template_name="inicio.html")),
+    url(r'^historia/?$', TemplateView.as_view(template_name="historia.html"), name='historia'),
 
 
 )

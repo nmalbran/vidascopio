40fa6eee16f98b2e134165be000d7efb6de60837
Nicolas Malbran <nmalbran@gmail.com>
2013-08-04 20:45:33 -0400

    Agregada pagina inicio.

--- web2/urls.py
+++ web2/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
-    url(r'^/?$', TemplateView.as_view(template_name="index.html")),
+    url(r'^/?$', TemplateView.as_view(template_name="inicio.html")),
 
 
 )

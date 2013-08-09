9dd2b0b0b7f63b3a4479a170e8d25972139b580f
Nicolas Malbran <nmalbran@gmail.com>
2013-08-09 19:43:47 -0400

    Agregada pagina normal.

--- web2/urls.py
+++ web2/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     url(r'^/?$', TemplateView.as_view(template_name="inicio.html")),
     url(r'^historia/?$', TemplateView.as_view(template_name="historia.html"), name='historia'),
-
+    url(r'^pagina/?$', TemplateView.as_view(template_name="pagina.html"), name='pagina'),
 
 )

efa60f653930c1ce128310295ef85f94ecf488ac
Nicolas Malbran <nmalbran@gmail.com>
2013-09-30 12:07:43 -0300

    Cambiado debug a false.

--- vidascopio/settings.py
+++ vidascopio/settings.py
@@ -3,7 +3,7 @@ import os
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -26,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vidascopio.herokuapp.com', 'localhost', '0.0.0.0']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name

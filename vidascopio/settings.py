b22cf7e32676d500d1573a0a8930e51535f0227f
Nicolas Malbran <nmalbran@gmail.com>
2013-09-30 12:28:59 -0300

    tercer paso: secret key por env.

--- vidascopio/settings.py
+++ vidascopio/settings.py
@@ -90,8 +90,8 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-SECRET_KEY = "asdasdasdasdoko"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+#SECRET_KEY = "asdasdasdasdoko"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (

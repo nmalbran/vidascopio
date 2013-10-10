ae1622d7d685cee3688dea49f535b3294d5f8de3
Nicolas Malbran <nmalbran@gmail.com>
2013-10-10 01:06:32 -0300

    Agregado formulario de contacto y envio de mails.

---
+++ web2/forms.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+class ContactoForm(forms.Form):
+    nombre = forms.CharField()
+    mail = forms.EmailField()
+    telefono = forms.CharField(label='Teléfono')
+    mensaje = forms.CharField(widget=forms.Textarea)
--- web2/views.py
+++ web2/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
-from django.views.generic import DetailView, TemplateView
+import os
+from postmark import PMMail
+from django.views.generic import DetailView, TemplateView, View
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.template import RequestContext
 
 from models import Pagina, Menu
+from forms import ContactoForm
 
 class PaginaView(DetailView):
     template_name = 'web2/pagina.html'
@@ -32,5 +37,35 @@ class QueOfrecemosView(TemplateView):
         return context
 
 
-class ContactoView(TemplateView):
+class ContactoView(View):
     template_name = 'web2/contacto.html'
+
+    def get(self, request):
+        form = ContactoForm()
+        return render_to_response(self.template_name, {'form': form}, context_instance=RequestContext(request))
+
+    def post(self, request):
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            body = """
+            Mensaje enviado desde vidascopio.cl
+
+            Nombre: %(nombre)s
+            Mail: %(mail)s
+            Telefono: %(telefono)s
+            Mensaje:
+            %(mensaje)s
+            """ % form.cleaned_data
+
+            message = PMMail(api_key = os.environ.get('POSTMARK_API_KEY'),
+                             subject = "Contacto desde Vidascopio.cl",
+                             sender = "contacto@vidascopio.cl",
+                             to = "nmalbran@gmail.com",
+                             text_body = body,
+                             tag = "contacto-web")
+
+            enviado = message.send()
+            if enviado:
+                return render_to_response('web2/contacto_fin.html', context_instance=RequestContext(request))
+
+        return render_to_response(self.template_name, {'form': form}, context_instance=RequestContext(request))
